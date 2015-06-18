0439e2ba006c901d82c3d6fddea42c604e1a922e
jakob <jakob.oswald@gmx.net>
2015-06-18 19:00:15 +0200

    Initial commit

---
+++ client/assets/js/app.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ])
+    .controller('mainController', MainController)
+    .config(config)
+    .run(run)
+  ;
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled:false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+  MainController.$inject = ['$log', 'FoundationApi'];
+  function MainController($log, FoundationApi) {
+      FoundationApi.subscribe('basicModal', function(event) {
+          $log.info('Modal ' + event);
+      });
+  }
+
+})();
